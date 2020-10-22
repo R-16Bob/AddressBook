fe747b4b055095685b97fc02adea32cd0e7206e5
R-16Bob <15343443062@163.com>
2020-10-22 22:49:01 +0800

    第一次升级
    1.升级SQLHelper中executeUpdate，改为使用PreparedStatement，提高效率和可读性，防止SQL注入；
    2.升级SQLHelper中的executeQuery,使其直接返回Object对象数组为元素的List，消除dao中的数据库操作；
    3.增加了一张效果截图。

--- src/dao/PersonDao.java
+++ src/dao/PersonDao.java
@@ -8,82 +8,70 @@ import pojo.Person;
 public class PersonDao {
 	// 添加 联系人
 	public void addPerson(Person p){ 
-		String sql="insert into persons values("+p.getPid()+",'"+
-	p.getName()+"','"+p.getSex()+"','"+p.getTel()+"','"+p.getAddress()
-	+"','"+p.getEmail()+"')";
-        SQLHelper. executeUpdate(sql);
+		String sql="insert into persons values(?,?,?,?,?,?)";
+        SQLHelper. executeUpdateByParameters(sql, p.getPid(),p.getName(),p.getSex(),p.getTel(),p.getAddress(),p.getEmail());
 	}	 
 	//通过人的编号删除联系人
 	public void deletePersonByID(int pid){ 		
-		String sql="delete from persons where pid="+pid;
-        SQLHelper. executeUpdate(sql);
+		String sql="delete from persons where pid=?";
+        SQLHelper. executeUpdateByParameters(sql, pid);
 	}
-	//通过人的姓名查找联系人，返回一个集合
-	public List<Person> queryPersonByName(String name){ 
-		List<Person> list=new ArrayList<Person>();
-		String sql="select * from persons where name='"+name+"'";
-		ResultSet rs=SQLHelper.executeQuery(sql);
-		try {
-			while(rs.next()) {
-				Person p = new Person();
-				p.setPid(rs.getInt(1));
-				p.setName(rs.getString(2));
-				p.setSex(rs.getString(3));
-				p.setTel(rs.getString(4));
-				p.setAddress(rs.getString(5));
-				p.setEmail(rs.getString(6));
-				list.add(p);
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		SQLHelper.closeConnection();
-		return list;		
-		}
 	//修改联系人姓名
 	public void updateNameByID(int pid,String newName){
-		String sql="update persons set name='"+newName+"' where pid="+pid;
-		SQLHelper.executeUpdate(sql);
+		String sql="update persons set name=? where pid=?";
+		SQLHelper.executeUpdateByParameters(sql, newName,pid);
 	}
 	//修改联系人性别
 	public void updateSexByID(int pid,String newSex){
-		String sql="update persons set sex='"+newSex+"' where pid="+pid;
-		SQLHelper.executeUpdate(sql);
+		String sql="update persons set sex=? where pid=?";
+		SQLHelper.executeUpdateByParameters(sql, newSex,pid);
 	}
 	//修改联系人电话
 	public void updateTelByID(int pid,String newTel){
-		String sql="update persons set tel='"+newTel+"' where pid="+pid;
-		SQLHelper.executeUpdate(sql);
+		String sql="update persons set tel=? where pid=?";
+		SQLHelper.executeUpdateByParameters(sql, newTel,pid);
 	}
 	//修改联系人地址
 	public void updateAddressByID(int pid,String newAdd){
-		String sql="update persons set address='"+newAdd+"' where pid="+pid;
-		SQLHelper.executeUpdate(sql);
+		String sql="update persons set address=? where pid=?";
+		SQLHelper.executeUpdateByParameters(sql, newAdd,pid);
 	}		
 	//修改联系人性别
 	public void updateEmailByID(int pid,String newEmail){
-		String sql="update persons set email='"+newEmail+"' where pid="+pid;
-		SQLHelper.executeUpdate(sql);
+		String sql="update persons set email=? where pid=?";
+		SQLHelper.executeUpdateByParameters(sql, newEmail,pid);
 	}
 	public List<Person> queryAllPerson(){  //查询全部记录
-		List<Person> list = new ArrayList<Person>();
+		List<Person> plist = new ArrayList<Person>();
 		String sql="select * from persons ";
-		ResultSet rs=SQLHelper.executeQuery(sql);
-		try {
-			while(rs.next()) {
-				Person p = new Person();
-				p.setPid(rs.getInt(1));
-				p.setName(rs.getString(2));
-				p.setSex(rs.getString(3));
-				p.setTel(rs.getString(4));
-				p.setAddress(rs.getString(5));
-				p.setEmail(rs.getString(6));
-				list.add(p);
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
+		List<Object[]> list=SQLHelper.executeQueryAsList(sql);
+		for(Object[] arr:list) {
+			Person person=new Person();
+			person.setPid(new Integer(arr[0].toString()));
+			person.setName(arr[1].toString());
+			person.setSex(arr[2].toString());
+			person.setTel(arr[3].toString());
+			person.setAddress(arr[4].toString());
+			person.setEmail(arr[5].toString());
+			plist.add(person);
 		}
-		SQLHelper.closeConnection();
-		return list;
+		return plist;
 	}
+	//通过人的姓名查找联系人，返回一个集合
+		public List<Person> queryPersonByName(String name){ 
+			List<Person> plist=new ArrayList<Person>();
+			String sql="select * from persons where name='"+name+"'";
+			List<Object[]> list=SQLHelper.executeQueryAsList(sql);
+			for(Object[] arr:list) {
+				Person person=new Person();
+				person.setPid(new Integer(arr[0].toString()));
+				person.setName(arr[1].toString());
+				person.setSex(arr[2].toString());
+				person.setTel(arr[3].toString());
+				person.setAddress(arr[4].toString());
+				person.setEmail(arr[5].toString());
+				plist.add(person);
+			}
+			return plist;		
+			}
 }
--- src/dbutil/SQLHelper.java
+++ src/dbutil/SQLHelper.java
@@ -2,8 +2,11 @@ package dbutil;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 
 public class SQLHelper {
 	private static String driver = "com.mysql.jdbc.Driver";
@@ -36,16 +39,53 @@ public class SQLHelper {
 			ex.printStackTrace();
 		}
 	}
-	public static int executeUpdate(String sql) {
-		int r=0;
-		try {
-			Connection conn = DriverManager.getConnection(url, user, pwd);
-			Statement cmd=conn.createStatement();
-			r=cmd.executeUpdate(sql);
-			conn.close();
-		} catch (Exception ex) {
-			ex.printStackTrace();
+	
+	//带参数的sql语句，一个就可以代替传统的增删改！!!
+		public static int executeUpdateByParameters(String sql,Object... params) {
+			int r=0;
+			try {
+				Connection conn = DriverManager.getConnection(url, user, pwd);
+				PreparedStatement pcmd=conn.prepareStatement(sql);
+				for(int i=0;i<params.length;i++) {
+					Object param=params[i];
+					if(param instanceof Integer)
+						pcmd.setInt(i+1,new Integer(param.toString()));
+					else if(param instanceof Double)
+						pcmd.setDouble(i+1, new Double(param.toString()));
+					else if(param instanceof Float)
+						pcmd.setFloat(i+1, new Float(param.toString()));
+					else if(param instanceof String)
+						pcmd.setString(i+1, param.toString());
+					else pcmd.setObject(i+1, param);
+				}
+				r=pcmd.executeUpdate();
+				conn.close();
+			} catch (Exception ex) {
+				ex.printStackTrace();
+			}
+			return r;
 		}
-		return r;
-	}
+		//不想在dao中看到数据库操作，不要返回结果集！返回List吧！
+		public static List<Object[]> executeQueryAsList(String sql){
+			List<Object[]> list=new ArrayList<Object[]>();
+			ResultSet rs=null;
+			try {
+				conn = DriverManager.getConnection(url, user, pwd);
+				Statement cmd=conn.createStatement();
+				rs=cmd.executeQuery(sql);
+				int cols=rs.getMetaData().getColumnCount();
+				while(rs.next()) {
+					//每一行记录就是一个Object数组
+					Object[] arr=new Object[cols];
+					for(int i=0;i<arr.length;i++) {
+						arr[i]=rs.getObject(i+1);
+					}
+					list.add(arr);  //将结果集的每一行都加入list
+				}
+				conn.close();
+			} catch (Exception ex) {
+				ex.printStackTrace();
+			}
+			return list;
+		}
 }
